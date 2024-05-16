6ab1a824b8545701ca59eb690460722b6b38e64f
Andrew Efimenko <anrybio@gmail.com>
2024-05-16 10:39:58 +0300

    Update

--- Assets/Scripts/Features/CollectingPoint/Systems/CollectingPointActivationSystem.cs
+++ Assets/Scripts/Features/CollectingPoint/Systems/CollectingPointActivationSystem.cs
@@ -1,4 +1,5 @@
 ﻿using Features.CollectingPoint.Components;
+using Features.Shared.Components;
 using Features.Shared.Systems;
 using Scellecs.Morpeh;
 
@@ -6,6 +7,7 @@ namespace Features.CollectingPoint.Systems
 {
     public class CollectingPointActivationSystem : UpdateSystemWithDistanceCheckWithPlayer
     {
+        private const float DefaultRadius = 3.5f;
         private Filter _filter;
         
         public override void OnAwake()
@@ -16,9 +18,16 @@ namespace Features.CollectingPoint.Systems
 
         public override void OnUpdate(float deltaTime)
         {
+            
             foreach (var e in _filter)
             {
-                if (CheckDistanceWithPlayer(e, 3.4f))
+                var radiusComp = e.GetComponent<RadiusColliderComponent>(out var isExist);
+                var radius = DefaultRadius;
+                
+                if (isExist)
+                    radius = radiusComp.Radius;
+                
+                if (CheckDistanceWithPlayer(e, radius))
                 {
                     if (!e.Has<CollectingPointActivatedComponent>())
                         e.AddComponent<CollectingPointActivatedComponent>();
